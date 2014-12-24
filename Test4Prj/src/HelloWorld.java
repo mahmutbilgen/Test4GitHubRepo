9902d47560609a096e35efde4bba332ee0f68787
mahmut.bilgen <mahmut.bilgen@gmail.com>
2014-12-24 10:00:27 +0200

    Second commint desc

---
+++ Test4Prj/src/HelloWorld.java
@@ -0,0 +1,15 @@
+
+/*
+ * 
+ */
+public class HelloWorld {
+/*
+ * Same text same text #
+ */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Test Message Test Message");
+
+	}
+
+}
