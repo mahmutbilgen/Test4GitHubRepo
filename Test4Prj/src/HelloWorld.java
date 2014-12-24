f7bf97f11326b893a6efbeb63e3eaa21526c43f5
mahmutbilgen <mahmutbilgen@users.noreply.github.com>
2014-12-24 10:36:35 +0200

    Update HelloWorld.java

--- Test4Prj/src/HelloWorld.java
+++ Test4Prj/src/HelloWorld.java
@@ -5,6 +5,7 @@
 public class HelloWorld {
 /*
  * Same text same text #
+ * Some extra extra sample text from web gui
  */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
